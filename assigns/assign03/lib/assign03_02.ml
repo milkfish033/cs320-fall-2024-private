eb974ef40ebc515292a4cce040f0a41704d3481d
milkfish033 <kerwin@milkfishs-super-flashy-Mac.local>
2024-09-26 10:56:17 -0400

    updated

--- assigns/assign03/lib/assign03_02.ml
+++ assigns/assign03/lib/assign03_02.ml
@@ -1,16 +1,11 @@
-let rec take n lst =
-  match lst, n with
-  | [], _ -> []
-  | _, 0 -> []
-  | x :: xs, n -> x :: (take (n - 1) xs)
-
-(* Function to generate the generalized Fibonacci sequence *)
 let gen_fib l k =
-  (* Helper function to compute the next element in the sequence *)
-  let rec aux seq k =
-    if k < List.length l then List.nth l k  (* If k is within the starting values, return the corresponding element *)
-    else
-      let sum = List.fold_left (+) 0 (take (List.length l) seq) in
-      aux (sum :: seq) (k - 1)
-  in
-  aux (List.rev l) k  (* Start the sequence with reversed initial values *)
+  let len_l = List.length l in
+    let rec aux acc n =
+      if n < len_l then List.nth l n
+      else match acc with
+        | [] -> failwith "Accumulator should never be empty"
+        | _ :: tail ->
+          let new_value = List.fold_left (+) 0 acc in
+          if n = k then new_value
+          else aux (tail @ [new_value]) (n + 1)
+    in aux l len_l
--- assigns/assign03/lib/assign03_03.ml
+++ assigns/assign03/lib/assign03_03.ml
@@ -1,30 +1,29 @@
-type tree =
+(* Tree definition *)
+type tree = 
   | Leaf of int
   | Node of tree list
 
 (* Helper function to compute the height of a tree *)
-let rec height t =
+let rec height t = 
   match t with
   | Leaf _ -> 0
-  | Node children ->
-      if children = [] then 0
-      else 1 + List.fold_left (fun acc child -> max acc (height child)) 0 children
+  | Node children -> 
+    if children = [] then 0
+    else 1 + List.fold_left (fun acc child -> max acc (height child)) 0 children
 
-(* Helper function to gather all terminal elements (leaves or empty nodes) of a subtree *)
+(* Helper function to extract all terminal elements from a tree *)
 let rec terminal_elements t =
   match t with
   | Leaf _ -> [t]
-  | Node children ->
-      if children = [] then [t]
-      else List.concat (List.map terminal_elements children)
+  | Node children -> 
+    if children = [] then [t]
+    else List.concat (List.map terminal_elements children)
 
-(* The collapse function *)
+(* Collapse function *)
 let rec collapse h t =
-  if h = 0 then t
-  else match t with
-    | Leaf _ -> t
+    match t with
+    | Leaf _ -> t  (* A leaf stays the same regardless of the collapse *)
     | Node children ->
-        if height t <= h then t
-        else Node (List.concat (List.map (fun child ->
-          if height child = h - 1 then terminal_elements child
-          else [collapse h child]) children))
+      if height t < h then t  (* No need to collapse if the tree is already shorter than h *)
+      else if h = 1 then Node (terminal_elements t)  (* Collapse to terminal elements *)
+      else Node (List.map (collapse (h - 1)) children)  (* Recursively collapse subtrees *)
