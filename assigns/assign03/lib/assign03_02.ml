896d2b8634bcd998509f30b0bb514b1989b0eb2f
milkfish033 <kerwin@milkfishs-super-flashy-Mac.local>
2024-09-26 18:58:06 -0400

    updated

--- assigns/assign03/lib/assign03_02.ml
+++ assigns/assign03/lib/assign03_02.ml
@@ -1,3 +1,11 @@
-let rec gen_fib l k= 
-  if k < List.length l then List.nth l k 
-  else gen_fib l (k-1) + gen_fib l (k-2)
+let gen_fib l k =
+  let len = List.length l in
+  if len = 0 || k < 0 then failwith "Invalid input"
+  else
+    let rec aux n acc =
+      if n < len then List.nth l n
+      else
+        let sum = List.fold_left (fun acc i -> acc + (aux (n - i) acc)) 0 (List.init len (fun x -> x + 1)) in
+        sum
+    in
+    aux k 0
