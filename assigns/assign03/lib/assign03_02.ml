2306335675e2cf820b2098f719119009d442b1c0
Kerwin <141232455+milkfish033@users.noreply.github.com>
2024-09-26 22:26:36 -0400

    Update assign03_02.ml

--- assigns/assign03/lib/assign03_02.ml
+++ assigns/assign03/lib/assign03_02.ml
@@ -1,1 +1,13 @@
+let sum_last_n lst n =
+    let last_n_elements = List.rev (List.take n (List.rev lst)) in
+    List.fold_left (+) 0 last_n_elements
 
+let gen_fib l k = 
+  let rec helper lst kth n acc =
+    if kth < List.length l then List.nth l kth  
+    else if n = kth then List.nth acc (List.length acc - 1) 
+    else 
+      let new_value = sum_last_n acc (List.length l) in
+      helper lst kth (n + 1) (acc @ [new_value] )
+  in
+  helper l k (List.length l - 1) l 
