6a609227eed24fc581faaa4f7d09bcb045300b4f
milkfish033 <kerwin@milkfishs-super-flashy-Mac.local>
2024-11-10 22:35:11 -0500

    still working on replace, subst, expr_of_val, and fun parse

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -3,25 +3,34 @@ open Utils
 open Stdlib320
 let parse = My_parser.parse
 
+  let expr_of_val v =
+    match v with 
+      | Ok(VFun (x,e)) -> Fun (x,e)
+      |
 
 
+  let rec replace_var x y e =
+    match e with 
+      |Var z -> if z = y then Var x else Var z
+      |App (e1,e2) -> App(replace_var x y e1, replace x y e2)
+      |Fun (z, e) -> Fun(z, replace_var x y e)
 
-  
   let rec subst x e1 e2 = 
-  match e2 with 
-    |Var y -> if x = y then x else Var y
+  match e2 with  
+    |Var y -> if e1 = y then expr_of_val x else Var y
     |Num _ | Unit | True | False -> e2
     |Bop (op, e21, e22) -> Bop(op, subst x e1 e21, subst x e1 e22)
     |App (e21, e22) -> App( subst x e1 e21, subst x e1 e22)
     |If (e21, e22, e23) -> If(subst x e1 e21, subst x e1 e22, subst x e1 e23)
     |Fun (y, body) ->
-      if x = y then e2 else Fun (y, subst x e1 body)
+      if e1 = y then Fun(y,body) 
+      else 
+        let z = gensym() in
+        Fun (z, subst x e1 (replace_var z y body))
     |Let (y, e21, e22) -> 
       let e21' = subst x e1 e21 in 
       let e22' = if x = y then e22 else subst x e1 e22 in 
       Let (y, e21', e22')
-
-
 
 
 let rec eval e = 
@@ -58,52 +67,28 @@ let rec eval e =
           | And, Ok(VBool v1), Ok(VBool v2) ->  Ok( VBool (v1 && v2))
           | Or, Ok(VBool v1), Ok(VBool v2) ->  Ok( VBool (v1 || v2))
           | _ -> Error(InvalidArgs op))
-      | Let(str, e1, e2) -> assert false   
+      | Let(str, e1, e2) -> eval(subst (eval e1) e2) 
       | Fun(str , e) -> Ok(VFun (str, e))
-      | App (e1, e2) -> (
-        let v1 = eval e1 in 
-        let v2 = eval e2 in 
-        match v1 with 
-         |Ok(VFun (x, body)) ->
-            eval (subst x v2 body)
+      | App (e1, e2) -> 
+        match eval e1 with  
+         |Ok(VFun (x, e)) ->
+            (match eval e2 with 
+            | Ok(VNum v2) -> eval(subst v2 x e)
+            | Ok(VBool v2) -> eval(subst v2 x e)
+            | Ok(VUnit) -> eval(subst  x e)
+            | Ok(VFun) -> assert false
+            | Error(x) ->  assert false
+            )
          |_ -> Error(InvalidApp)
-      )
+      
 
 
-(*
-    | Fun (x, body) -> Fun(x, body)
-    | App (e1, e2) -> (
-      let v1 = eval e1 in 
-      let v2 = eval e2 in 
-      match v1 with 
-        |Fun (x, body) -> eval (subst x v2 body)
-      )
-*)
-
-
-   (* | Let (x, e1, e2) -> 
-        let v1 = eval e1 in 
-        eval (subst x v1 e2)
-    | Ifte (guard, bThen, bElse) -> 
-        (match eval guard with 
-          | Bool true -> eval bThen 
-          | Bool false -> eval eElse
-          | _ -> failwith ("type error"))
-    | Fun (x, body) -> Fun(x, body)
-    | App (e1, e2) -> (
-      let v1 = eval e1 in 
-      let v2 = eval e2 in 
-      match v1 with 
-        |Fun (x, body) -> eval (subst x v2 body)
-        | _ -> failwith ("type error in function application")
-      )
-    | _ -> failwith "unimplemented"
-
-
-let interp fname =
-  let c = open_in fname in
-  let m = Parser.parse c in
-  let v = eval m in
-  v
-  
-*)
+let interp str =
+let* prog = parse str in
+let expr = desugar prog in
+  eval expr
+        
+let interp' str =
+match interp str with
+    | Some v -> string_of_expr (expr_of_val v)
+    | None -> "ERROR"
