384126f69b39efc436caada4d94e7763e1bfdb2b
milkfish033 <kerwin@crc-dot1x-nat-10-239-103-196.bu.edu>
2024-11-25 16:23:25 -0500

    let left

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -13,17 +13,20 @@ val eval : expr -> value
 
 val interp : string -> (value, error) result*) 
 
+exception AssertFail
+exception DivByZero
+
 
  let rec desugar_value s = 
     match s with 
-        |SUnit -> Unit
-        |STrue -> True
-        |SFalse -> False
-        |SNum x -> Num x
-        |SVar x -> Var x
-        |SIf (a, b, c) -> If (desugar_value a, desugar_value b, desugar_value c)
-        |SBop (op, left, right) -> Bop (op, desugar_value left, desugar_value right)
-        |SAssert expr -> Assert (desugar_value expr)
+        | SUnit -> Unit
+        | STrue -> True
+        | SFalse -> False
+        | SNum x -> Num x
+        | SVar x -> Var x
+        | SIf (a, b, c) -> If (desugar_value a, desugar_value b, desugar_value c)
+        | SBop (op, left, right) -> Bop (op, desugar_value left, desugar_value right)
+        | SAssert expr -> Assert (desugar_value expr)
         | SFun { arg = (arg_name, arg_ty); args; body } ->
             List.fold_right
               (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
@@ -49,83 +52,177 @@ let rec desugar prog =
     match prog with
         | [] -> Unit
         | ({is_rec = b; name = x; args = a; ty= t; value = e} ):: ls -> 
-            Let {is_rec = b; name= x; ty = t; value = desugar_value e ;body = desugar ls}
+            let desugared_value =
+                List.fold_right
+                  (fun (arg_name, arg_ty) acc -> Fun (arg_name, arg_ty, acc))
+                  a
+                  (desugar_value e)
+              in
+              Let
+                { is_rec = b
+                ; name = x
+                ; ty = t
+                ; value = desugared_value
+                ; body = desugar ls
+                }
             
 
 
 (*expr -> (ty, error) result*)
 let rec type_of ctxt =
-    let rec go = function
-        | Unit -> Ok(UnitTy)
-        | True -> Ok(BoolTy)
-        | False -> Ok(BoolTy)
-        | Num _ -> Ok(IntTy)
-        | Var x -> Error(UnknownVar x)
-        | If (e1, e2, e3) -> (
-            match go e1, go e2, go e3 with
-            | Ok(BoolTy), Ok(t2), Ok(t3)  -> (
-                if t2 = t3 then Ok(t2)
-                else Error(IfTyErr(t2,t3))
+        let rec go = function
+            | Unit -> Ok(UnitTy)
+            | True -> Ok(BoolTy)
+            | False -> Ok(BoolTy)
+            | Num _ -> Ok(IntTy)
+            | Var x ->  (match List.assoc_opt x ctxt with 
+                |Some t -> Ok(t)
+                |_ -> Error(UnknownVar x)
+            )
+            | If (e1, e2, e3) -> (
+                match go e1, go e2, go e3 with
+                | Ok(BoolTy), Ok(t2), Ok(t3)  -> (
+                    if t2 = t3 then Ok(t2)
+                    else Error(IfTyErr(t2,t3))
+                    )
+                | Ok(t), _,_ -> Error(IfCondTyErr(t))
+                | _ -> assert false
                 )
-            | Ok(t), _,_ -> Error(IfCondTyErr(t))
-            | _ -> Error(ParseErr)
-            )
-        |Assert e -> 
-            (match type_of e with 
-                | Ok(BoolTy) -> Ok(BoolTy)
-                | Ok(t)-> Error(AssertTyErr(t))
-                | _ -> Error(ParseErr) 
-            )
-        | Bop (op, x,y) -> (
-            match op, x, y with 
-                | Add, e1, e2 -> go_op Add IntTy e1 e2
-                | Sub, e1, e2 -> go_op Sub IntTy e1 e2
-                | Mul, e1, e2 -> go_op Mul IntTy e1 e2 
-                | Div, e1, e2 -> go_op Div IntTy e1 e2
-                | Mod, e1, e2 -> go_op Mod IntTy e1 e2
-                | And, e1, e2 -> go_op And BoolTy e1 e2 
-                | Or, e1, e2 -> go_op Or BoolTy e1 e2 
-                | Lt, e1, e2 -> go_op_1 Lt BoolTy e1 e2 
-                | Lte, e1, e2 -> go_op_1 Lte BoolTy e1 e2 
-                | Gt, e1, e2 -> go_op_1 Gt BoolTy e1 e2 
-                | Gte, e1, e2 -> go_op_1 Gte BoolTy e1 e2 
-                | Eq, e1, e2 -> go_op_1 Eq BoolTy e1 e2 
-                | Neq, e1, e2 -> go_op_1 Neq BoolTy e1 e2 
+            |Assert e -> 
+                (match go e with 
+                    | Ok(BoolTy) -> Ok(BoolTy)
+                    | Ok(t)-> Error(AssertTyErr(t))
+                    | _ -> assert false 
                 )
-        | Fun (_, ty, e) -> (
-            match go e with 
-                |Ok(t) -> if t = ty then Ok(FunTy(ty,t))
-                            else Error(FunArgTyErr(ty, t))
-                |_ -> Error(ParseErr)
+            | Bop (op, x,y) -> (
+                match op, x, y with 
+                    | Add, e1, e2 -> go_op Add IntTy e1 e2
+                    | Sub, e1, e2 -> go_op Sub IntTy e1 e2
+                    | Mul, e1, e2 -> go_op Mul IntTy e1 e2 
+                    | Div, e1, e2 -> go_op Div IntTy e1 e2
+                    | Mod, e1, e2 -> go_op Mod IntTy e1 e2
+                    | And, e1, e2 -> go_op And BoolTy e1 e2 
+                    | Or, e1, e2 -> go_op Or BoolTy e1 e2 
+                    | Lt, e1, e2 -> go_op_1 Lt BoolTy e1 e2 
+                    | Lte, e1, e2 -> go_op_1 Lte BoolTy e1 e2 
+                    | Gt, e1, e2 -> go_op_1 Gt BoolTy e1 e2 
+                    | Gte, e1, e2 -> go_op_1 Gte BoolTy e1 e2 
+                    | Eq, e1, e2 -> go_op_1 Eq BoolTy e1 e2 
+                    | Neq, e1, e2 -> go_op_1 Neq BoolTy e1 e2 
+                    )
+            | Fun (_, ty, e) -> (
+                match go e with 
+                    |Ok(t) -> if t = ty then Ok(FunTy(ty,t))
+                                else Error(FunArgTyErr(ty, t))
+                    |_ -> assert false
+                )
+            | App (e1, e2) -> (
+                match go e1, go e2 with
+                    | (Ok(FunTy (ty_arg, ty_out))), t2 when Ok(ty_arg) = t2 -> Ok(ty_out)
+                    | Ok(t),_ -> Error(FunAppTyErr(t))
+                    | _ -> assert false
+                )
+            | Let {is_rec = false; name = x; ty = t; value = e1; body = e2} -> (
+                match go e1 with
+                | Ok(t1) when t = t1 ->  type_of  ((x, t) :: ctxt) e2
+                | _ -> assert false
             )
-        | App (e1, e2) -> (
+            | Let {is_rec = true; name = x; ty = t; value = e1; body = e2} -> (
+                match type_of ((x, t)::ctxt) e1  with
+                | Ok(t1) ->  if t <> t1 then Error(LetTyErr(t,t1)) else(
+                    match type_of((x,t) :: ctxt) e2 with
+                        |Ok typ -> Ok typ
+                        | Error e-> Error e )
+                        |Error e -> Error e  
+            )
+        and go_op op ty_out e1 e2 =
             match go e1, go e2 with
-                | (Ok(FunTy (ty_arg, ty_out))), t2 when Ok(ty_arg) = t2 -> Ok(ty_out)
-                | Ok(t),_ -> Error(FunAppTyErr(t))
-                | _ -> Error(ParseErr)
-            )
-        | Let (x, ty, e1, e2) -> (
-            match go e1 with
-            | Ok(t1) when ty = t1 -> type_of ((x, ty)) e2
-            | _ -> None
+            | Ok t1, Ok t2 when t1 = ty_out && t2 = ty_out -> Ok(ty_out)
+            | Ok t1, Ok t2 when t2 = ty_out -> Error (OpTyErrL (op, ty_out, t1))
+            | Ok t1, Ok t2 when t1 = ty_out -> Error (OpTyErrR (op, ty_out, t2))
+            | _, _ -> Error(ParseErr)
+        
+        and go_op_1 op ty_out e1 e2 =
+            match go e1, go e2 with
+            | Ok t1, Ok t2 when t1 = IntTy && t2 = IntTy -> Ok(ty_out)
+            | Ok t1, Ok t2 when t2 = IntTy -> Error (OpTyErrL (op, ty_out, t1))
+            | Ok t1, Ok t2 when t1 = IntTy -> Error (OpTyErrR (op, ty_out, t2))
+            | _, _ -> Error(ParseErr)
+
+        in go 
+
+let type_of = type_of []
+
+    (*val eval : expr -> value*)
+let rec eval en =
+    let rec go = function
+        | Unit -> VUnit
+        | True -> VBool true
+        | False -> VBool false
+        | Var x -> (match Env.find_opt x en with 
+            |Some v -> v
+            | _ -> assert false )
+        | Num n -> VNum n
+        |Assert e -> (
+            match go e with 
+                | VBool true -> VUnit
+                | _ -> raise AssertFail
         )
-    and go_op op ty_out e1 e2 =
-        match go e1, go e2 with
-        | Ok t1, Ok t2 when t1 = ty_out && t2 = ty_out -> Ok(ty_out)
-        | Ok t1, Ok t2 when t2 = ty_out -> Error (OpTyErrL (op, ty_out, t1))
-        | Ok t1, Ok t2 when t1 = ty_out -> Error (OpTyErrR (op, ty_out, t2))
-        | _, _ -> Error(ParseErr)
-    
-    and go_op_1 op ty_out e1 e2 =
-        match go e1, go e2 with
-        | Ok t1, Ok t2 when t1 = IntTy && t2 = IntTy -> Ok(ty_out)
-        | Ok t1, Ok t2 when t2 = IntTy -> Error (OpTyErrL (op, ty_out, t1))
-        | Ok t1, Ok t2 when t1 = IntTy -> Error (OpTyErrR (op, ty_out, t2))
-        | _, _ -> Error(ParseErr)
+        | Fun (x, _, e) -> VClos {name = None; arg =x; body = e; env = en}
+        | Bop(op,e1, e2) -> (
+            match op, e1, e2 with
+                | Add ,e1, e2 -> go_op (+) e1 e2
+                | Sub ,e1, e2 -> go_op (-) e1 e2
+                | Mul ,e1, e2 -> go_op ( * ) e1 e2
+                | Eq (e1, e2) -> (
+            match go e1 with
+            | Some (VNum m) -> (
+                match go e2 with
+                | Some (VNum n) -> Some (VBool (m = n))
+                | _ -> None
+            )
+            | _ -> None
+            ))
+        | If (e1, e2, e3) -> (
+             match go e1 with
+                | VBool true -> go e2
+                | VBool false -> go e3
+                | _ -> assert false
+            )
+        | Let {is_rec = false; name = x; ty = t; value = e1; body = e2} -> (
+            if (type_of e1) <> Ok(t) then assert false 
+            else
+            match go e1 with
+                | v -> eval (Env.add x v en) e2
+        )
+        | App (e1, e2) -> (
+            match go e1 with
+                | VClos {name = f; arg = x; body = b; env = fun_env} -> (
+                    match f with 
+                        | None -> (match go e2 with
+                            | v -> eval (Env.add x v fun_env) b)
 
-    in go 
+                        | Some f -> (match go e2 with
+                        | v ->
+                            let env = Env.add f (VClos {name = Some f; arg = x; body = b; env = fun_env}) fun_env in
+                            let env = Env.add x v env in
+                            eval env b)
+                )
+                |_ -> failwith "Invalid"
+           
+        )
+        | Let{is_rec = true; name = x; ty = t; value = e1; body = e2} ->
+            eval (Env.add f (VClos {name = f;arg = x; body =e1; env = e}) en) e2
+        and go_op op e1 e2 =
+            match go e1 with
+                | VNum m -> (
+                    match go e2 with
+                        | VNum n -> VNum (op m n)
+                        | _ -> assert false
+                )
+            | _ -> assert false
 
-    let type_of = type_of []
 
-
- 
+        in go
+        
+    let eval = eval Env.empty
